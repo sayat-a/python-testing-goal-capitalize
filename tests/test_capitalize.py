3480926cce8e7e28a768149043ca20221e7b8578
sayat-a <168581637+sayat-a@users.noreply.github.com>
2024-06-12 16:20:59 +0000

    modify test

--- tests/test_capitalize.py
+++ tests/test_capitalize.py
@@ -8,10 +8,8 @@
 
 from capitalize import capitalize
 
-if capitalize('hello') != 'Hello':
-    raise Exception('Функция работает неверно!')
+assert capitalize('hello') == 'Hello'
 
-if capitalize('') != '':
-    raise Exception('Функция работает неверно!')
+assert capitalize('') == ''
 
 print('Все тесты пройдены!')
